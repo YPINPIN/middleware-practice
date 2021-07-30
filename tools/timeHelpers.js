b7695ca07f9f12ab4d6c8d829dfc0af718cd316b
YPINPIN <yping611@gmail.com>
2021-07-30 15:50:58 +0800

    [REFACTOR] Modify timeHelpers

--- tools/timeHelpers.js
+++ tools/timeHelpers.js
@@ -1,7 +1,16 @@
-// 格式化時間
+// 格式化日期、時間：yyyy-MM-dd HH:mm:ss
 function formatTime(time) {
-  const timeString = `${time.getFullYear()}-${padStart(time.getMonth() + 1)}-${padStart(time.getDate())} ${padStart(time.getHours())}:${padStart(time.getMinutes())}:${padStart(time.getSeconds())}`
-  return timeString
+  return `${toDateString(time)} ${toTimeString(time)}`
+}
+
+// 格式化日期：yyyy-MM-dd
+function toDateString(time) {
+  return `${time.getFullYear()}-${padStart(time.getMonth() + 1)}-${padStart(time.getDate())}`
+}
+
+// 格式化時間：HH:mm:ss
+function toTimeString(time) {
+  return `${padStart(time.getHours())}:${padStart(time.getMinutes())}:${padStart(time.getSeconds())}`
 }
 
 // 十位數補零
@@ -10,5 +19,7 @@ function padStart(number) {
 }
 
 module.exports = {
-  formatTime
+  formatTime,
+  toDateString,
+  toTimeString
 }
